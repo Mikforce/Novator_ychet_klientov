4a7a3b8f3743cc45e811cfc194a22d4d6440d12a
Ivan <100131490+Mikforce@users.noreply.github.com>
2023-05-10 15:54:24 +0300

    7
    7

--- nov/myapp/views.py
+++ nov/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Group, Coach, Administrator, Subscription, Client
 
 
@@ -27,6 +27,8 @@ def add_client(request):
 def client_list(request):
     clients = Client.objects.all()
     return render(request, 'client_list.html', {'clients': clients})
+
+
 
 
 
